be508871ffa88d0c0a112858ebffa80f2d836f64
Shannon Rachal <srachal674@gmail.com>
2024-03-27 16:57:14 -0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,22 +1,16 @@
 function playMeasures2and3 () {
     // Play measure 2
     music.play(music.tonePlayable(294, calculateDottedDuration(music.beat(BeatFraction.Quarter))), music.PlaybackMode.UntilDone)
-    music.rest(calculateDottedDuration(music.beat(BeatFraction.Eighth)))
+    music.rest(calculateDottedDuration(music.beat(BeatFraction.Quarter)))
     music.play(music.tonePlayable(220, calculateDottedDuration(music.beat(BeatFraction.Quarter))), music.PlaybackMode.UntilDone)
-    music.rest(calculateDottedDuration(music.beat(BeatFraction.Eighth)))
+    music.rest(calculateDottedDuration(music.beat(BeatFraction.Quarter)))
     music.play(music.tonePlayable(294, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
-    music.rest(calculateDottedDuration(music.beat(BeatFraction.Eighth)))
+    music.rest(calculateDottedDuration(music.beat(BeatFraction.Quarter)))
     music.rest(music.beat(BeatFraction.Quarter))
     music.play(music.tonePlayable(247, music.beat(BeatFraction.Eighth)), music.PlaybackMode.UntilDone)
     music.play(music.tonePlayable(262, music.beat(BeatFraction.Eighth)), music.PlaybackMode.UntilDone)
     music.rest(music.beat(BeatFraction.Eighth))
     music.play(music.tonePlayable(277, music.beat(BeatFraction.Eighth)), music.PlaybackMode.UntilDone)
-    // Play measure 3
-    music.play(music.tonePlayable(247, music.beat(BeatFraction.Eighth)), music.PlaybackMode.UntilDone)
-    music.play(music.tonePlayable(262, music.beat(BeatFraction.Eighth)), music.PlaybackMode.UntilDone)
-    music.rest(music.beat(BeatFraction.Eighth))
-    music.play(music.tonePlayable(277, music.beat(BeatFraction.Eighth)), music.PlaybackMode.UntilDone)
-    music.rest(music.beat(BeatFraction.Quarter))
 }
 // Declare the variable at the global scope
 // Define the function to calculate dotted duration
