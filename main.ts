3baae76e72b33daf2dfdd6aa7b19e15732e760d4
Shannon Rachal <srachal674@gmail.com>
2024-03-28 10:11:36 -0700

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,12 +1,111 @@
+// Measure 9
+// Measure 10
+function playMeasures6to19 () {
+    // This function plays measures 6 - 19 and will be used in other functions.
+    // Measure 6
+    music.play(music.tonePlayable(294, calculateDottedDuration(music.beat(BeatFraction.Quarter))), music.PlaybackMode.UntilDone)
+    music.rest(calculateDottedDuration(music.beat(BeatFraction.Quarter)))
+    music.play(music.tonePlayable(220, calculateDottedDuration(music.beat(BeatFraction.Quarter))), music.PlaybackMode.UntilDone)
+    music.rest(calculateDottedDuration(music.beat(BeatFraction.Quarter)))
+    // Measure 7
+    music.play(music.tonePlayable(294, calculateDottedDuration(music.beat(BeatFraction.Quarter))), music.PlaybackMode.UntilDone)
+    music.rest(calculateDottedDuration(music.beat(BeatFraction.Quarter)))
+    music.rest(music.beat(BeatFraction.Eighth))
+    music.rest(music.beat(BeatFraction.Eighth))
+    // This is a D-flat
+    music.play(music.tonePlayable(277, music.beat(BeatFraction.Eighth)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(262, music.beat(BeatFraction.Eighth)), music.PlaybackMode.UntilDone)
+    music.rest(music.beat(BeatFraction.Eighth))
+    music.play(music.tonePlayable(247, music.beat(BeatFraction.Eighth)), music.PlaybackMode.UntilDone)
+    // Measure 8
+    // This is a B-flat
+    music.play(music.tonePlayable(233, music.beat(BeatFraction.Eighth)), music.PlaybackMode.UntilDone)
+    music.rest(calculateDottedDuration(music.beat(BeatFraction.Quarter)))
+    music.play(music.tonePlayable(349, calculateDottedDuration(music.beat(BeatFraction.Quarter))), music.PlaybackMode.UntilDone)
+    music.rest(calculateDottedDuration(music.beat(BeatFraction.Quarter)))
+    // Measure 9
+    // This is a B-flat
+    music.play(music.tonePlayable(277, music.beat(BeatFraction.Eighth)), music.PlaybackMode.UntilDone)
+    music.rest(calculateDottedDuration(music.beat(BeatFraction.Quarter)))
+    music.rest(music.beat(BeatFraction.Quarter))
+    music.play(music.tonePlayable(247, music.beat(BeatFraction.Eighth)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(262, music.beat(BeatFraction.Eighth)), music.PlaybackMode.UntilDone)
+    music.rest(music.beat(BeatFraction.Eighth))
+    music.play(music.tonePlayable(277, music.beat(BeatFraction.Eighth)), music.PlaybackMode.UntilDone)
+    // Measure 10
+    music.play(music.tonePlayable(294, calculateDottedDuration(music.beat(BeatFraction.Quarter))), music.PlaybackMode.UntilDone)
+    music.rest(calculateDottedDuration(music.beat(BeatFraction.Quarter)))
+    music.play(music.tonePlayable(220, calculateDottedDuration(music.beat(BeatFraction.Quarter))), music.PlaybackMode.UntilDone)
+    music.rest(calculateDottedDuration(music.beat(BeatFraction.Quarter)))
+    // Measure 11
+    music.play(music.tonePlayable(294, calculateDottedDuration(music.beat(BeatFraction.Quarter))), music.PlaybackMode.UntilDone)
+    music.rest(calculateDottedDuration(music.beat(BeatFraction.Quarter)))
+    music.rest(music.beat(BeatFraction.Quarter))
+    music.play(music.tonePlayable(262, music.beat(BeatFraction.Eighth)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(277, music.beat(BeatFraction.Eighth)), music.PlaybackMode.UntilDone)
+    music.rest(music.beat(BeatFraction.Eighth))
+    music.play(music.tonePlayable(294, music.beat(BeatFraction.Eighth)), music.PlaybackMode.UntilDone)
+    // Measure 12
+    // This is a E-flat
+    music.play(music.tonePlayable(311, calculateDottedDuration(music.beat(BeatFraction.Quarter))), music.PlaybackMode.UntilDone)
+    music.rest(calculateDottedDuration(music.beat(BeatFraction.Quarter)))
+    // This is a B-flat
+    music.play(music.tonePlayable(233, calculateDottedDuration(music.beat(BeatFraction.Quarter))), music.PlaybackMode.UntilDone)
+    music.rest(calculateDottedDuration(music.beat(BeatFraction.Quarter)))
+    // Measure 13
+    // This is a E-flat
+    music.play(music.tonePlayable(311, calculateDottedDuration(music.beat(BeatFraction.Quarter))), music.PlaybackMode.UntilDone)
+    music.rest(calculateDottedDuration(music.beat(BeatFraction.Quarter)))
+    music.rest(music.beat(BeatFraction.Quarter))
+    music.play(music.tonePlayable(247, music.beat(BeatFraction.Eighth)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(262, music.beat(BeatFraction.Eighth)), music.PlaybackMode.UntilDone)
+    music.rest(music.beat(BeatFraction.Eighth))
+    music.play(music.tonePlayable(277, music.beat(BeatFraction.Eighth)), music.PlaybackMode.UntilDone)
+    // Measure 14
+    music.play(music.tonePlayable(294, calculateDottedDuration(music.beat(BeatFraction.Quarter))), music.PlaybackMode.UntilDone)
+    music.rest(calculateDottedDuration(music.beat(BeatFraction.Quarter)))
+    music.play(music.tonePlayable(220, calculateDottedDuration(music.beat(BeatFraction.Quarter))), music.PlaybackMode.UntilDone)
+    music.rest(calculateDottedDuration(music.beat(BeatFraction.Quarter)))
+    // Measure 15
+    music.play(music.tonePlayable(294, calculateDottedDuration(music.beat(BeatFraction.Quarter))), music.PlaybackMode.UntilDone)
+    music.rest(calculateDottedDuration(music.beat(BeatFraction.Quarter)))
+    music.rest(music.beat(BeatFraction.Quarter))
+    // This is a D-flat
+    music.play(music.tonePlayable(277, music.beat(BeatFraction.Eighth)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(262, music.beat(BeatFraction.Eighth)), music.PlaybackMode.UntilDone)
+    music.rest(music.beat(BeatFraction.Eighth))
+    music.play(music.tonePlayable(247, music.beat(BeatFraction.Eighth)), music.PlaybackMode.UntilDone)
+    // Measure 16
+    // This is a B-flat
+    music.play(music.tonePlayable(233, music.beat(BeatFraction.Eighth)), music.PlaybackMode.UntilDone)
+    music.rest(calculateDottedDuration(music.beat(BeatFraction.Quarter)))
+    music.play(music.tonePlayable(349, calculateDottedDuration(music.beat(BeatFraction.Quarter))), music.PlaybackMode.UntilDone)
+    music.rest(calculateDottedDuration(music.beat(BeatFraction.Quarter)))
+    // Measure 17
+    // This is B-flat
+    music.play(music.tonePlayable(233, calculateDottedDuration(music.beat(BeatFraction.Quarter))), music.PlaybackMode.UntilDone)
+    music.rest(calculateDottedDuration(music.beat(BeatFraction.Quarter)))
+    music.rest(music.beat(BeatFraction.Eighth))
+    music.rest(music.beat(BeatFraction.Eighth))
+    music.play(music.tonePlayable(247, music.beat(BeatFraction.Eighth)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(262, music.beat(BeatFraction.Eighth)), music.PlaybackMode.UntilDone)
+    music.rest(music.beat(BeatFraction.Eighth))
+    music.play(music.tonePlayable(277, music.beat(BeatFraction.Eighth)), music.PlaybackMode.UntilDone)
+    // Measure 18
+    music.play(music.tonePlayable(294, calculateDottedDuration(music.beat(BeatFraction.Quarter))), music.PlaybackMode.UntilDone)
+    music.rest(calculateDottedDuration(music.beat(BeatFraction.Quarter)))
+    music.rest(calculateDottedDuration(music.beat(BeatFraction.Double)))
+    // Measure 19
+    music.rest(music.beat(BeatFraction.Breve))
+}
 function playMeasures2and3 () {
-    //This is played 2 times and covers measueres 2 - 5 in the music.
+    // This is played 2 times and covers measueres 2 - 5 in the music.
     // Play measure 2
     music.play(music.tonePlayable(294, calculateDottedDuration(music.beat(BeatFraction.Quarter))), music.PlaybackMode.UntilDone)
     music.rest(calculateDottedDuration(music.beat(BeatFraction.Quarter)))
     music.play(music.tonePlayable(220, calculateDottedDuration(music.beat(BeatFraction.Quarter))), music.PlaybackMode.UntilDone)
     music.rest(calculateDottedDuration(music.beat(BeatFraction.Quarter)))
-    
-    //Measure 3
+    // Measure 3
     music.play(music.tonePlayable(294, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
     music.rest(calculateDottedDuration(music.beat(BeatFraction.Quarter)))
     music.rest(music.beat(BeatFraction.Quarter))
@@ -14,6 +113,12 @@ function playMeasures2and3 () {
     music.play(music.tonePlayable(262, music.beat(BeatFraction.Eighth)), music.PlaybackMode.UntilDone)
     music.rest(music.beat(BeatFraction.Eighth))
     music.play(music.tonePlayable(277, music.beat(BeatFraction.Eighth)), music.PlaybackMode.UntilDone)
+}
+function playMeasures621 () {
+    playMeasures6to19()
+    music.play(music.tonePlayable(294, calculateDottedDuration(music.beat(BeatFraction.Quarter))), music.PlaybackMode.UntilDone)
+    music.rest(calculateDottedDuration(music.beat(BeatFraction.Quarter)))
+    music.play(music.tonePlayable(220, calculateDottedDuration(music.beat(BeatFraction.Quarter))), music.PlaybackMode.UntilDone)
 }
 // Declare the variable at the global scope
 // Define the function to calculate dotted duration
